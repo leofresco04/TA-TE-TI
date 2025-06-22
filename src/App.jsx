7a7488f27605377e6b4cc27e2fddb52d19caa09b
leofresco04 <leofresco78@gmail.com>
2025-06-22 02:28:54 -0300

    busqueda del ganador

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,93 @@
 import { useState } from "react"
 import Square from "./Components/Square"
 import "./index.css"
-
+//declaramos los elementos que van a tener los turnos 
 const TURNS={
   X: "x",
   O: "o"
 }
+//conmbos de ganandores
+  const winnerCombos =[
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,4,8],
+    [1,4,7],
+    [0,3,6],
+    [2,5,7],
+    [2,4,6],
+    
+
+  ]
+
 function App() {
+  //le damos null a todos los casilleros
   const [board, setBoard] = useState (
     Array(9).fill(null)
   )
 
+//verificamos los ganadores para ver si x u o ganó
+const check =(boardCheck)=>{
+  for( const combo of winnerCombos){
+    const [a,b,c] = combo
+    if(
+      boardCheck[a] &&
+      boardCheck[a] === boardCheck[b] &&
+      boardCheck [a] === boardCheck[c]
+    ){
+      return boardCheck[a]
+    } }
+    return null
+  
+  
+}
 
+//iniciamos el turno en x
   const [turn, setTurn] = useState(TURNS.X)
   
+ //buscamos ganador
+ const [winner, setWinner] = useState(null)// null es que no hay ganandor, false empate y true ganador
+
   const updateBoard =(index)=>{
+    //preguntamos si la posicion teine algo, si, si tiene, no sigue y si hay ganador tampoco.
+    if(board[index] || winner) return
+    //con spred operetor hacemos una copia de board
     const newBoard = [...board]
+    //aqui le damos el valor que tenia board al momento de clickkear sea x u o
     newBoard[index] = turn
+    // y aqui ese valor lo abosrbe board para renderizarlo en square con board[index]
     setBoard(newBoard)
-     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
+    //aqui verificamos de quien es el turno, si turn está en x al clickear pasa a o y viceversa
+    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
+    //aqui turn absorbe el valor x u o que se le de a newTrun
      setTurn(newTurn)
-     console.log(turn)
+     //revisar si hay ganador, newBoard le da valor a const check =(boardCheck) de arriba el cual busca ganador en los combos
+    const newWinner =  check(newBoard)
+    if(
+      newWinner
+    )//verificamos si es null o si devuelve un ganador en check
+    {  setWinner(newWinner)
+      alert("el ganador es " + newWinner)
+    
+    }
+    
+    
   }
 
  
 
   return (
     <main className="board">
-      <h1>TA TE TI</h1>
+      <h1>TA TE TI of leofresco04</h1>
       <section className="game">
         {board.map((turn, index) => (
-          <Square 
+          <Square
           key={index}
           updateBoard={updateBoard}
-          index={index}>   
+          index={index}> 
+
           {board[index]}
+
            </Square>
         ))}
       </section>
